8207ad18d9c4684780d0d3e5ba42c0be52884d88
BillyMiles <williammiles201@gmail.com>
2022-01-07 20:06:03 -0500

    resorts seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Resort.create(name:"Ascutney", zipcode: "05037")
+Resort.create(name:"Bear Creek", zipcode: "05056")
+Resort.create(name:"Bolton Valley", zipcode: "05477")
+Resort.create(name:"Bromley", zipcode: "05152")
+Resort.create(name:"Burke", zipcode: "05832")
+Resort.create(name:"Cochran", zipcode: "05477")
+Resort.create(name:"Jay Peak", zipcode: "05859")
+Resort.create(name:"Killington", zipcode: "05751")
+Resort.create(name:"Lyndon", zipcode: "05851")
+Resort.create(name:"Mad Rover Glen", zipcode: "05673")
+Resort.create(name:"Magic", zipcode: "05148")
+Resort.create(name:"Middlebury Snow Bowl", zipcode: "05748")
+Resort.create(name:"Mount Snow", zipcode: "05356")
+Resort.create(name:"Okemo", zipcode: "05149")
+Resort.create(name:"Northeast Slopes", zipcode: "05040")
+Resort.create(name:"Pico", zipcode: "05701")
+Resort.create(name:"Smuggler's Notch", zipcode: "05464")
+Resort.create(name:"Stowe", zipcode: "05672")
+Resort.create(name:"Stratton", zipcode: "05360")
+Resort.create(name:"Sugarbush", zipcode: "05674")
+Resort.create(name:"Suicide Six", zipcode: "05091")
+
+puts "done seeding"
